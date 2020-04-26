c7434bebd21d8824b38759df0fb0bed6fa644f2f
ashwini82 <ashwiniatharv1982@gmail.com>
2020-04-26 16:51:46 -0400

    Updates

---
+++ precache-manifest.910ca8c21c94d1a88130d73c6f094808.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1e73a5be684ef27980a31d0edd581c",
+    "url": "/Weather2/index.html"
+  },
+  {
+    "revision": "9cdcaee65c2088c9dc70",
+    "url": "/Weather2/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1fc98c77f2aed433d76b",
+    "url": "/Weather2/static/css/main.21f96981.chunk.css"
+  },
+  {
+    "revision": "9cdcaee65c2088c9dc70",
+    "url": "/Weather2/static/js/2.152e45fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather2/static/js/2.152e45fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fc98c77f2aed433d76b",
+    "url": "/Weather2/static/js/main.899650ba.chunk.js"
+  },
+  {
+    "revision": "832a2a827e06aed77680",
+    "url": "/Weather2/static/js/runtime-main.47a5bb40.js"
+  }
+]);
